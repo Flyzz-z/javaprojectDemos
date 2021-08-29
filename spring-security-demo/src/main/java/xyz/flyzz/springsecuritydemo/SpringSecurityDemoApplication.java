bebd947dba275446463731f80fd7f4cb12706ec6
flyzz-z <1353530978@qq.com>
2021-08-29 16:25:45 +0800

    spring-security-demo

---
+++ spring-security-demo/src/main/java/xyz/flyzz/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.flyzz.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+
+}
